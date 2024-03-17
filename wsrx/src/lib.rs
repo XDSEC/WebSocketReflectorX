ea3d438a366e0bc6c7c0d3ed7ae8b54c6aaf47aa
Reverier-Xu <reverier.xu@woooo.tech>
2024-03-17 23:54:29 +0800

    :art: thanks to rustfmt

--- wsrx/src/lib.rs
+++ wsrx/src/lib.rs
@@ -1,6 +1,7 @@
 //! WebSocket Reflector X
 //!
-//! A simple crate that proxies pure TCP connections to WebSocket connections and vice versa.
+//! A simple crate that proxies pure TCP connections to WebSocket connections
+//! and vice versa.
 
 pub mod proxy;
 
