1387eb5c9a28d74ee45fafdccf8eb3bd1c5297e5
Reverier-Xu <reverier.xu@woooo.tech>
2024-04-08 21:43:19 +0800

    :construction: try github workflows

---
+++ deploy_linux.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+export APP_NAME="WebSocketReflectorX"
+export APP_VERSION=0.2.1
+export GIT_VERSION=$(git rev-parse --short HEAD)
+
+echo "> $APP_NAME packager (Linux x86_64) [v$APP_VERSION]"
+
+## CHECKS ######################################################################
+
+if [ "$(id -u)" == "0" ]; then
+  echo "This script MUST NOT be run as root" 1>&2
+  exit 1
+fi
+
+if [ ${PWD##*/} != $APP_NAME ]; then
+  echo "This script MUST be run from the $APP_NAME/ directory"
+  exit 1
+fi
+
+## SETTINGS ####################################################################
+
+use_contribs=false
+make_install=false
+create_package=false
+upload_package=false
+
+while [[ $# -gt 0 ]]
+do
+case $1 in
+  -c|--contribs)
+  use_contribs=true
+  ;;
+  -i|--install)
+  make_install=true
+  ;;
+  -p|--package)
+  create_package=true
+  ;;
+  -u|--upload)
+  upload_package=true
+  ;;
+  *)
+  echo "> Unknown argument '$1'"
+  ;;
+esac
+shift # skip argument or value
+done
+
+## COPY FILES ##################################################################
+
+mkdir -p dist
+APP_ROOT=dist
+install -D ./build/bin/wsrx "${APP_ROOT}/usr/bin/wsrx"
+install -D ./build/bin/wsrx-desktop "${APP_ROOT}/usr/bin/wsrx-desktop"
+install -Dm644 "./freedesktop/tech.woooo.wsrx.desktop" "$APP_ROOT"/usr/share/applications/tech.woooo.wsrx.desktop
+install -Dm644 "./freedesktop/tech.woooo.wsrx.svg" "$APP_ROOT"/usr/share/icons/hicolor/scalable/apps/tech.woooo.wsrx.svg
+
+## DEPLOY ######################################################################
+
+if [[ $use_contribs = true ]] ; then
+  export LD_LIBRARY_PATH=$(pwd)/contribs/src/env/linux_x86_64/usr/lib/:/usr/lib
+else
+  export LD_LIBRARY_PATH=/usr/lib/
+fi
+
+echo '---- Prepare linuxdeploy + plugins'
+
+unset LD_LIBRARY_PATH; #unset QT_PLUGIN_PATH; #unset QTDIR;
+
+USRDIR=/usr;
+if [ -d bin/usr/local ]; then
+  USRDIR=/usr/local
+fi
+if [ -z "$QTDIR" ]; then
+  QTDIR=/usr/lib/qt
+fi
+
+if [ ! -x contribs/deploy/linuxdeploy-x86_64.AppImage ]; then
+  wget -c -nv "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" -P contribs/deploy/
+  wget -c -nv "https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage" -P contribs/deploy/
+  wget -c -nv "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage" -P contribs/deploy/
+fi
+chmod a+x contribs/deploy/linuxdeploy-x86_64.AppImage
+chmod a+x contribs/deploy/linuxdeploy-plugin-appimage-x86_64.AppImage
+chmod a+x contribs/deploy/linuxdeploy-plugin-qt-x86_64.AppImage
+
+# linuxdeploy settings
+export EXTRA_QT_PLUGINS="svg;"
+
+## PACKAGE (AppImage) ##########################################################
+
+if [[ $create_package = true ]] ; then
+  echo '---- Running AppImage packager'
+  ./contribs/deploy/linuxdeploy-x86_64.AppImage --appdir build/bin --plugin qt --output appimage
+  mv $APP_NAME-x86_64.AppImage $APP_NAME-$APP_VERSION-linux64.AppImage
+
+  #echo '---- Installation directory content recap (after linuxdeploy):'
+  #find bin/
+fi
+
+## UPLOAD ######################################################################
+
+if [[ $upload_package = true ]] ; then
+  printf "---- Uploading to transfer.sh"
+  curl --upload-file $APP_NAME*.AppImage https://transfer.sh/$APP_NAME-$APP_VERSION-git$GIT_VERSION-linux64.AppImage
+  printf "\n"
+fi
---
+++ deploy_windows.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+export APP_NAME="WebSocketReflectorX"
+export APP_VERSION=0.2.1
+export GIT_VERSION=$(git rev-parse --short HEAD)
+
+echo "> $APP_NAME packager (Windows x86_64) [v$APP_VERSION]"
+
+## CHECKS ######################################################################
+
+if [ ${PWD##*/} != $APP_NAME ]; then
+  echo "This script MUST be run from the $APP_NAME/ directory"
+  exit 1
+fi
+
+## SETTINGS ####################################################################
+
+use_contribs=false
+make_install=false
+create_package=false
+upload_package=false
+
+while [[ $# -gt 0 ]]
+do
+case $1 in
+  -c|--contribs)
+  use_contribs=true
+  ;;
+  -i|--install)
+  make_install=true
+  ;;
+  -p|--package)
+  create_package=true
+  ;;
+  -u|--upload)
+  upload_package=true
+  ;;
+  *)
+  echo "> Unknown argument '$1'"
+  ;;
+esac
+shift # skip argument or value
+done
+
+## APP INSTALL #################################################################
+
+if [[ $make_install = true ]] ; then
+  echo '---- Running make install'
+  mkdir -p dist
+  APP_ROOT=dist
+  cp ./build/bin/wsrx.exe "${APP_ROOT}/wsrx.exe"
+  cp ./build/bin/wsrx-desktop.exe "${APP_ROOT}/wsrx-desktop.exe"
+
+  #echo '---- Installation directory content recap (after make install):'
+  #find bin/
+fi
+
+## DEPLOY ######################################################################
+
+echo '---- Running windeployqt'
+windeployqt $APP_ROOT/ --qmldir ./desktop/ui --qmldir ./desktop/components
+
+#echo '---- Installation directory content recap (after windeployqt):'
+#find bin/
+
+mv $APP_ROOT $APP_NAME
+
+## PACKAGE (zip) ###############################################################
+
+if [[ $create_package = true ]] ; then
+  echo '---- Compressing package'
+  7z a $APP_NAME-$APP_VERSION-win64.zip $APP_NAME
+fi
+
+## PACKAGE (NSIS) ##############################################################
+
+if [[ $create_package = true ]] ; then
+  echo '---- Creating installer'
+  mv $APP_NAME windows/$APP_NAME
+  makensis windows/setup.nsi
+  mv windows/*.exe $APP_NAME-$APP_VERSION-win64.exe
+fi
+
+## UPLOAD ######################################################################
+
+if [[ $upload_package = true ]] ; then
+  printf "---- Uploading to transfer.sh"
+  curl --upload-file $APP_NAME*.zip https://transfer.sh/$APP_NAME-$APP_VERSION-git$GIT_VERSION-win64.zip
+  printf "\n"
+  curl --upload-file $APP_NAME*.exe https://transfer.sh/$APP_NAME-$APP_VERSION-git$GIT_VERSION-win64.exe
+  printf "\n"
+fi
