f26a285e6d1a6d4c277d659e140618223acc2813
Reverier-Xu <reverier.xu@woooo.tech>
2024-05-28 15:39:20 +0800

    :bug: fix macos build

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -6,8 +6,6 @@ export GIT_VERSION=$(git rev-parse --short HEAD)
 
 echo "> $APP_NAME packager (Linux x86_64) [v$APP_VERSION]"
 
-## CHECKS ######################################################################
-
 if [ "$(id -u)" == "0" ]; then
   echo "This script MUST NOT be run as root" 1>&2
   exit 1
@@ -18,51 +16,15 @@ if [ ${PWD##*/} != $APP_NAME ]; then
   exit 1
 fi
 
-## SETTINGS ####################################################################
-
-use_contribs=false
-make_install=false
-create_package=false
-upload_package=false
-
-while [[ $# -gt 0 ]]
-do
-case $1 in
-  -c|--contribs)
-  use_contribs=true
-  ;;
-  -i|--install)
-  make_install=true
-  ;;
-  -p|--package)
-  create_package=true
-  ;;
-  -u|--upload)
-  upload_package=true
-  ;;
-  *)
-  echo "> Unknown argument '$1'"
-  ;;
-esac
-shift # skip argument or value
-done
-
-## COPY FILES ##################################################################
-
 mkdir -p dist
 APP_ROOT=dist
+strip ./build/bin/wsrx
 install -D ./build/bin/wsrx "${APP_ROOT}/usr/bin/wsrx"
 install -D ./build/bin/wsrx-desktop "${APP_ROOT}/usr/bin/wsrx-desktop"
 install -Dm644 "./freedesktop/tech.woooo.wsrx.desktop" "$APP_ROOT"/usr/share/applications/tech.woooo.wsrx.desktop
 install -Dm644 "./freedesktop/tech.woooo.wsrx.svg" "$APP_ROOT"/usr/share/icons/hicolor/scalable/apps/tech.woooo.wsrx.svg
 
-## DEPLOY ######################################################################
-
-if [[ $use_contribs = true ]] ; then
-  export LD_LIBRARY_PATH=$(pwd)/contribs/src/env/linux_x86_64/usr/lib/:/usr/lib
-else
-  export LD_LIBRARY_PATH=/usr/lib/
-fi
+export LD_LIBRARY_PATH=$(pwd)/contribs/src/env/linux_x86_64/usr/lib/:/usr/lib
 
 echo '---- Prepare linuxdeploy + plugins'
 
@@ -85,22 +47,10 @@ chmod a+x contribs/deploy/linuxdeploy-x86_64.AppImage
 chmod a+x contribs/deploy/linuxdeploy-plugin-appimage-x86_64.AppImage
 chmod a+x contribs/deploy/linuxdeploy-plugin-qt-x86_64.AppImage
 
-# linuxdeploy settings
 export EXTRA_QT_PLUGINS="svg;"
 export EXTRA_PLATFORM_PLUGINS="libqxcb.so;libqwayland-egl.so;libqwayland-generic.so"
 export QML_SOURCES_PATHS="desktop/ui:desktop/components"
-## PACKAGE (AppImage) ##########################################################
 
-if [[ $create_package = true ]] ; then
-  echo '---- Running AppImage packager'
-  ./contribs/deploy/linuxdeploy-x86_64.AppImage --appdir $APP_ROOT --plugin qt --output appimage
-  mv $APP_NAME-x86_64.AppImage $APP_NAME-$APP_VERSION-linux64.AppImage
-fi
-
-## UPLOAD ######################################################################
-
-if [[ $upload_package = true ]] ; then
-  printf "---- Uploading to transfer.sh"
-  curl --upload-file $APP_NAME*.AppImage https://transfer.sh/$APP_NAME-$APP_VERSION-git$GIT_VERSION-linux64.AppImage
-  printf "\n"
-fi
+echo '---- Running AppImage packager'
+./contribs/deploy/linuxdeploy-x86_64.AppImage --appdir $APP_ROOT --plugin qt --output appimage
+mv $APP_NAME-x86_64.AppImage $APP_NAME-$APP_VERSION-linux64.AppImage
