6cfdb2898ccf2cc4d57946022b37279be012debd
Reverier-Xu <reverier.xu@woooo.tech>
2024-07-26 20:27:45 +0800

    :tada: 0.2.19

--- deploy_windows.sh
+++ deploy_windows.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export APP_NAME="WebSocketReflectorX"
-export APP_VERSION=0.2.18
+export APP_VERSION=0.2.19
 export GIT_VERSION=$(git rev-parse --short HEAD)
 
 echo "> $APP_NAME packager (Windows x86_64) [v$APP_VERSION]"
