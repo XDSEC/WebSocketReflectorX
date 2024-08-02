9ba517f20e74cbc6d67b7ccb95af6524f801d494
Reverier-Xu <reverier.xu@woooo.tech>
2024-08-02 17:59:12 +0800

    :tada: 0.2.22

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export APP_NAME="WebSocketReflectorX"
-export APP_VERSION=0.2.21
+export APP_VERSION=0.2.22
 export GIT_VERSION=$(git rev-parse --short HEAD)
 
 echo "> $APP_NAME packager (macOS x86_64) [v$APP_VERSION]"
